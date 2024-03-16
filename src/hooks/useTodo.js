a7384a1817015ccbbac25dd67810970f1c928eeb
zelkovaria <hyein396@gmail.com>
2024-03-16 19:39:44 +0900

    feat: useTodo hook 추가

---
+++ src/hooks/useTodo.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const useTodo = (id) => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/todos/${id}`
+        );
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+  return { data, isLoading, error };
+};
+
+export default useTodo;
